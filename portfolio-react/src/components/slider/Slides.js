76a0d11329b586945465c22abba2271ad031dcdb
AndreS27893 <andrescba27893@gmail.com>
2022-03-27 10:20:22 -0300

    Ultimos cambios
    cambie las fotos del slides

--- portfolio-react/src/components/slider/Slides.js
+++ portfolio-react/src/components/slider/Slides.js
@@ -4,19 +4,19 @@ import "./Slider.css";
 const slidesInfo = [
   {
     src:
-      "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_960_720.jpg",
+      "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
     alt: "Project 1",
     desc: "Project 1",
   },
   {
     src:
-      "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
+      "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
     alt: "Project 2",
     desc: "Project 2",
   },
   {
     src:
-      "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
+      "https://images.pexels.com/photos/4955393/pexels-photo-4955393.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
     alt: "Project 3",
     desc: "Project 3",
   },
